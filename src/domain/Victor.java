d0e5033cfdf4aa181ae4a7b15333560d525c7bf2
MAQ31I <MAQ31I@PC31>
2024-11-08 14:35:20 -0500

    victor class

---
+++ src/domain/Victor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Victor {
+    private int cedula ;
+    int edad;
+    String nombre;
+}
