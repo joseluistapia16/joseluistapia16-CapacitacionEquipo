c0d555fd4c2404cea31385387427567bc207dfcc
joseluistapia16 <joseluistapia09@gmail.com>
2024-11-09 19:41:20 -0500

    Subida MySql Connector

--- src/capacitacionproyecto/CapacitacionProyecto.java
+++ src/capacitacionproyecto/CapacitacionProyecto.java
@@ -11,18 +11,45 @@ import domain.Pelicula;
 import domain.Persona;
 import domain.Vehiculo;
 import domain.estudiante;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Jose Luis Tapia
  */
 public class CapacitacionProyecto {
-/* Cambio 6 de Noviembre 2024*/
-    static int u=0;
+
+    /* Cambio 6 de Noviembre 2024*/
+    static int u = 0;
     static String direccion;
+
+
+    public static Connection Conexion(String base) {
+        Connection Conect = null;
+        try {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String str = "jdbc:mysql://localhost:3306/" + base;
+                Conect = DriverManager.getConnection(str, "root", "1234");
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(CapacitacionProyecto.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CapacitacionProyecto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Conect;
+
+    }
+
     public static void main(String[] args) {
-        carro();
-        Versionew();
+        var con = Conexion("fichas_medicas_personal");
+        System.out.println(con);
+//        carro();
+//        Versionew();
         //Lombok();
         //herencia();
         //instancias();
@@ -43,61 +70,62 @@ public class CapacitacionProyecto {
 //        System.out.println("Resultado:"+r);
 //        System.out.println(getSuma(10, 8));
     }
-    private static void carro(){
-    var obj=new Carro( 4,"Gris",20,5,"Audi");
+
+    private static void carro() {
+        var obj = new Carro(4, "Gris", 20, 5, "Audi");
         System.out.println(obj.toString());
     }
-      private static void Versionew(){
-      Carro vob = new Carro();
-      vob.setLlantas(4);
-      vob.setColor("Blanco");
-      vob.setPernos(500);
-      vob.setAsientos(5);
-      vob.setMarca("Audi");
-      System.out.println(vob.toString());
-      }
-      
- 
-    
-    
-    private static void prueba2(){
-        var obj = new Pelicula(1, "Jason X", 120.89, "Terror",2005);
+
+    private static void Versionew() {
+        Carro vob = new Carro();
+        vob.setLlantas(4);
+        vob.setColor("Blanco");
+        vob.setPernos(500);
+        vob.setAsientos(5);
+        vob.setMarca("Audi");
+        System.out.println(vob.toString());
+    }
+
+    private static void prueba2() {
+        var obj = new Pelicula(1, "Jason X", 120.89, "Terror", 2005);
         System.out.println(obj.toString());
     }
-    
-    public static int getSuma(int x,int y){
-        u=45;
-        return x+y;        
+
+    public static int getSuma(int x, int y) {
+        u = 45;
+        return x + y;
     }
-    public static void inicio(){
+
+    public static void inicio() {
         double precio;
-        var num =0;
+        var num = 0;
         var valor = 0.0;
-        var nombre ="";
+        var nombre = "";
         var res = true;
-        u=90;
+        u = 90;
         System.out.println("Hola ");
     }
-    
-   static void tipos(){
-       String cad;
-       Double valor=100.0;
+
+    static void tipos() {
+        String cad;
+        Double valor = 100.0;
         int a;
         double b;
-        byte c=7;
+        byte c = 7;
         boolean k;
         long num;
         byte m;
-        char car='k';
+        char car = 'k';
         System.out.println(c);
-        num=90;
+        num = 90;
         cad = "Curso";
-        System.out.println("long:"+num+" "+car);
-        System.out.println("referenciado "+cad.toUpperCase()+
-                " "+valor.toString());
+        System.out.println("long:" + num + " " + car);
+        System.out.println("referenciado " + cad.toUpperCase()
+                + " " + valor.toString());
     }
-  static void instancias(){
-      // Prueba
+
+    static void instancias() {
+        // Prueba
 //       Persona obP = new Persona();
 //       obP.setCedula("03939376");
 //       obP.setNombre("Jose Lopez");
@@ -113,39 +141,39 @@ public class CapacitacionProyecto {
 //       System.out.println("***************");
 //       var obP2 = new Persona("03938", "Jorge Gonzalez",56);
 //       System.out.println(obP2.toString());
-   }
-  
-  
-  private static void herencia(){
+    }
+
+    private static void herencia() {
 //      Persona obj = new Persona("0333773", "Jose Lopez", 56);
 //      System.out.println(obj.toString());
-      Empleado obj1 = new Empleado("037252", "Carlos Perez",67,1200.0,"Sistemas");
-      System.out.println(obj1.toString());
-      obj1.ejemplo();
-      Docente obj2 = new Docente("072726", "Miriam Gonzalez", 45, 
-              "Informatica", "Robotica");
-      System.out.println(obj2.toString());
-      obj2.ejemplo();
-  }
- 
-  private static void Lombok(){
-      Vehiculo vob = new Vehiculo();
-      vob.setMatricula("GHJ0987");
-      vob.setMarca("Toyota");
-      vob.setModelo("2023");
-      vob.setN_velocidades(5);
-      System.out.println(vob.toString());
-      Vehiculo obj = new Vehiculo("PML0876", "Datsun",
-              "2018", 6);
-      System.out.println(obj.toString());
-      // Lombok
-  }
-}
-class Prueba{
-   public static int x=60;
-   public static void metodo(){
-       System.out.println("Metodo llamado directamente");
-   }
+        Empleado obj1 = new Empleado("037252", "Carlos Perez", 67, 1200.0, "Sistemas");
+        System.out.println(obj1.toString());
+        obj1.ejemplo();
+        Docente obj2 = new Docente("072726", "Miriam Gonzalez", 45,
+                "Informatica", "Robotica");
+        System.out.println(obj2.toString());
+        obj2.ejemplo();
+    }
+
+    private static void Lombok() {
+        Vehiculo vob = new Vehiculo();
+        vob.setMatricula("GHJ0987");
+        vob.setMarca("Toyota");
+        vob.setModelo("2023");
+        vob.setN_velocidades(5);
+        System.out.println(vob.toString());
+        Vehiculo obj = new Vehiculo("PML0876", "Datsun",
+                "2018", 6);
+        System.out.println(obj.toString());
+        // Lombok
+    }
 }
 
+class Prueba {
 
+    public static int x = 60;
+
+    public static void metodo() {
+        System.out.println("Metodo llamado directamente");
+    }
+}
