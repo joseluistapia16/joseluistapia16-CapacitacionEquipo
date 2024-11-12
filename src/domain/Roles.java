465baece02139706b67d7348722e4d5ecf7f4a85
user <user@PC14>
2024-11-12 12:22:09 -0500

    Roles Pablo Garcia 12/11/24 12:22

--- src/domain/Roles.java
+++ src/domain/Roles.java
@@ -7,29 +7,31 @@ package domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@RequiredArgsConstructor
 
-/**
+/** Pablo Garcia 12/11/2024 12:06
  *
  * @author userasdasdasd
  */
 public class Roles {
 
-    private int id_rol;
-    String nombre;
-    String id_usuario;
-    String estado;
-
-    public Roles(String nombre, String id_usuario, String estado) {
-        this.nombre = nombre;
-        this.id_usuario = id_usuario;
-        this.estado = estado;
+    private Integer id_rol;
+    @NonNull
+    private String nombre;
+    @NonNull
+    private String id_usuario;
+    @NonNull
+    private String estado;
+  
+   
     }
     
     
-}
